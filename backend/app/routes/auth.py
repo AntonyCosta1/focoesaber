7c379c97156aa06683ee3e1283690c97da0ea975
Antony Costa <antonycosta210@gmail.com>
2026-04-27 14:13:40 -0300

    Ajuste no login oAuth2

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from app.database import get_db
 from app.models.user import User
-from app.schemas.auth import TokenResponse, LoginRequest
+from app.schemas.auth import TokenResponse
 from app.core.security import verify_password, criar_token_de_acesso
 
 router = APIRouter(prefix="/login", tags=["login"])
@@ -11,12 +12,12 @@ router = APIRouter(prefix="/login", tags=["login"])
 
 @router.post("/login", response_model=TokenResponse)
 def login(
-    credentials: LoginRequest,
+    form_data: OAuth2PasswordRequestForm = Depends(),
     db: Session = Depends(get_db)
 ):
-    user = db.query(User).filter(User.email == credentials.email).first()
+    user = db.query(User).filter(User.email == form_data.username).first()
 
-    if not user or not verify_password(credentials.senha, user.senha):
+    if not user or not verify_password(form_data.password, user.senha):
         raise HTTPException(status_code=401, detail="Email ou senha inválidos")
 
     access_token = criar_token_de_acesso({
